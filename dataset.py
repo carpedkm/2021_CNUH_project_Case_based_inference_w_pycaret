8838cafe8a4694ff05a6dd770779bd55db2cf145
carpedkm <flytodk@gmail.com>
2021-02-23 11:46:49 +0900

    init update catboost with target strok

--- dataset.py
+++ dataset.py
@@ -3,4 +3,5 @@ from pycaret import classification
 
 data_classification = pd.read_csv('./db_GIST.csv')
 classification_setup = classification.setup(data= data_classification, target='strok')
-tune_catboost = classification.tune_model('catboost')
+tune_catboost = classification.tune_model('xgboost')
+tune_catboost.to_csv('./data.csv')
